486d944d49963cb9a511a00adb9ce1c034e47693
Carles Brunet - @medhax <46396911+medhax@users.noreply.github.com>
2020-10-15 00:20:07 +0200

    Update menuadmin.sh

--- menuadmin.sh
+++ menuadmin.sh
@@ -15,7 +15,7 @@ do
 	    echo "Introdueix la contrasenya de l'usuari a crear"
 	    read PASSWD
 	 sudo useradd -c $NOMCOMPLET -m $NOMUSUARI
-echo $NOMUSUARI+":"+$PASSWD | chpasswd
+echo $NOMUSUARI:$PASSWD | chpasswd
 		sudo mkdir /home/${NOMUSUARI,,}/ftp
 		sudo chown nobody:nogroup /home/${NOMUSUARI,,}/ftp
 		sudo chmod a-w /home/${NOMUSUARI,,}/ftp 
