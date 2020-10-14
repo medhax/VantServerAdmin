a2d1642ee6222751f2618bcdd3a676898995a71a
Carles Brunet - @medhax <46396911+medhax@users.noreply.github.com>
2020-10-15 00:16:51 +0200

    Update menuadmin.sh

--- menuadmin.sh
+++ menuadmin.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-# Bash Menu Script Example
+# Script administratiu Carles Brunet
 
-PS3='Please enter your choice: '
+PS3='Què vols fer?: '
 options=("Crear un nou usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
 select opt in "${options[@]}"
 do
     case $opt in
         "Crear un nou usuari")
 		
-            echo "Introdueix el nom de l'usuari"
+            echo "Introdueix el nom d'usuari a crear"
 	    read NOMUSUARI
-	    sudo adduser $NOMUSUARI
-		sudo mkdir /home/${NOMUSUARI,,}
+	    echo "Introdueix el nom complet de l'usuari a crear"
+	    read NOMCOMPLET
+	    echo "Introdueix la contrasenya de l'usuari a crear"
+	    read PASSWD
+	 sudo useradd -c $NOMCOMPLET -m $NOMUSUARI
+echo $NOMUSUARI+":"+$PASSWD | chpasswd
 		sudo mkdir /home/${NOMUSUARI,,}/ftp
 		sudo chown nobody:nogroup /home/${NOMUSUARI,,}/ftp
 		sudo chmod a-w /home/${NOMUSUARI,,}/ftp 
