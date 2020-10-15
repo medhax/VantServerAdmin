4c50f4c5c4f7cb21adf6371b3292c872a474da6b
Carles Brunet - @medhax <46396911+medhax@users.noreply.github.com>
2020-10-15 09:52:43 +0200

    Update menuadmin.sh

--- menuadmin.sh
+++ menuadmin.sh
@@ -2,17 +2,17 @@
 # Script administratiu Carles Brunet
 
 PS3='Què vols fer?: '
-options=("Crear un nou usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
+options=("Crear un nou usuari" "Eliminar un usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
 select opt in "${options[@]}"
 do
     case $opt in
         "Crear un nou usuari")
 		
-            echo "Introdueix el nom d'usuari a crear"
+            echo "--> Introdueix el nom d'usuari a crear"
 	    read NOMUSUARI
-	    echo "Introdueix el nom complet de l'usuari a crear"
+	    echo "--> Introdueix el nom complet de l'usuari a crear"
 	    read NOMCOMPLET
-	    echo "Introdueix la contrasenya de l'usuari a crear"
+	    echo "--> Introdueix la contrasenya de l'usuari a crear"
 	    read PASSWD
 	 sudo useradd -c $NOMCOMPLET -m $NOMUSUARI
 sudo echo "$NOMUSUARI:$PASSWD" | sudo chpasswd
@@ -25,6 +25,11 @@ sudo echo "$NOMUSUARI:$PASSWD" | sudo chpasswd
 		git stash && git pull
 		sudo chmod +x ./menuadmin.sh
 		./menuadmin.sh
+            ;;
+	     "Eliminar un usuari")
+            echo "--> Introdueix el nom de l'usuari a eliminar: "
+	    read NOMUSUARIDEL
+		sudo userdel $NOMUSUARIDEL
             ;;
         "Obtenir informació interessant")
             echo "--> Llistat d'usuaris"
