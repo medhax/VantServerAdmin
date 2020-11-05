76c9f40de7dca3bad9943ebdf1450b1da329ff3e
Carles Brunet - @medhax <46396911+medhax@users.noreply.github.com>
2020-11-06 00:58:36 +0100

    Update menuadmin.sh

--- menuadmin.sh
+++ menuadmin.sh
@@ -2,7 +2,7 @@
 # Script administratiu Carles Brunet
 
 PS3='Què vols fer?: '
-options=("Crear un nou usuari" "Eliminar un usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
+options=("Crear els usuaris" "Eliminar un usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -17,8 +17,11 @@ chage -d 0 $user
 	 sudo useradd -c $user -m $user
 sudo echo "$user:$user" | sudo chpasswd
 		sudo mkdir /home/${user,,}/ftp
+		sudo mkdir /var/www/alumnes/${user,,}
 		sudo chown nobody:nogroup /home/${user,,}/ftp
 		sudo chmod a-w /home/${user,,}/ftp 
+		sudo chown nobody:nogroup /var/www/alumnes/${user,,}
+		sudo chmod a-w /var/www/alumnes/${user,,}
 		sudo echo "$user" >> /etc/vsftpd.chroot_list
 		done
             ;;
