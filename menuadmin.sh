9a49a83799e37bc9d194da72a36b82171fa9ea0f
Carles Brunet - @medhax <46396911+medhax@users.noreply.github.com>
2020-11-13 10:04:38 +0100

    Update menuadmin.sh

--- menuadmin.sh
+++ menuadmin.sh
@@ -2,7 +2,7 @@
 # Script administratiu Carles Brunet
 
 PS3='Què vols fer?: '
-options=("Crear els usuaris" "Eliminar un usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Sortir")
+options=("Crear els usuaris" "Eliminar un usuari" "Actualitzar aquest programa" "Obtenir informació interessant" "Comprovar permisos" "Sortir")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -18,13 +18,12 @@ chage -d 0 "$user"
 sudo echo "$user:$user" | sudo chpasswd
 		sudo mkdir /home/"${user,,}"/ftp
 		sudo mkdir /var/www/alumnes/"${user,,}"
-		sudo chown nobody:nogroup /home/"${user,,}"/ftp
+		sudo chown nobody:alumnes /home/"${user,,}"/ftp
 		sudo chmod a-w /home/"${user,,}"/ftp 
-		sudo chown nobody:nogroup /var/www/alumnes/"${user,,}"
+		sudo chown nobody:alumnes /var/www/alumnes/"${user,,}"
 		sudo chmod a-w /var/www/alumnes/"${user,,}"
 		unlink /home/"${user,,}"/ftp/"${user,,}"
 		mount --bind /var/www/alumnes/"${user,,}" /home/"${user,,}"/ftp
-
 		sudo echo "$user" >> /etc/vsftpd.chroot_list
 		done
             ;;
@@ -33,6 +32,21 @@ sudo echo "$user:$user" | sudo chpasswd
 		git stash && git pull
 		sudo chmod +x ./menuadmin.sh
 		./menuadmin.sh
+            ;;
+	    "Comprovar permisos")
+           	echo "--> Comprovant permisos..."
+		for user in $(more ../user-list.txt)
+		do
+		echo "$user"
+		echo "--> Establint permisos de carpetes"
+		sudo chmod -R 777 /var/www
+		sudo usermod -a -G alumnes "$user"
+		sudo chmod -R 777 /var/www
+		sudo chown nobody:alumnes /home/"${user,,}"/ftp
+		sudo chmod a-w /home/"${user,,}"/ftp 
+		sudo chown nobody:alumnes /var/www/alumnes/"${user,,}"
+		sudo chmod a-w /var/www/alumnes/"${user,,}"
+		done
             ;;
 	     "Eliminar un usuari")
             echo "--> Introdueix el nom de l'usuari a eliminar: "
